87a3d224e8d0af83c519074c668bf6becc14551e
laliko-omiadze <lalo.omiadze@gmail.com>
2024-03-13 14:24:11 +0000

    davasrule davaleba

---
+++ script.js
@@ -0,0 +1,23 @@
+let userNumber = 100
+
+if ( userNumber < 50) {
+    console.log ( "less than 50");
+} else if ( userNumber > 20) {
+    console.log ("greater than 20");
+} else ("error");
+
+
+let Name= "mariami"
+
+let result = Name = "mariami" ? "true" : "false"
+
+console.log (result)
+
+const userName = "mariami"
+switch (userName) {
+    case "mariami":
+    console.log("true");
+    break;
+default:
+    console.log ("false")
+}
